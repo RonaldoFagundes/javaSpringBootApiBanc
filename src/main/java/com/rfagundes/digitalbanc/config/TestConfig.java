57d1b269c8cbdce758ac59a3e359edb72c209c27
RonaldoFagundes <ronaldofagundes@gmail.com>
2022-10-31 12:22:33 -0300

    config properties add db_h2_file

--- src/main/java/com/rfagundes/digitalbanc/config/TestConfig.java
+++ src/main/java/com/rfagundes/digitalbanc/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.rfagundes.digitalbanc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,12 +13,17 @@ import com.rfagundes.digitalbanc.service.DBService;
 public class TestConfig {
 
 	@Autowired
-	private DBService dbService ;
+	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;	
 	
 	@Bean
-	public void instanciaBaseDeDados() {
-		this.dbService.instanciaBaseDeDados();
+	public boolean instanciaBaseDeDados() {
+		if(strategy.equals("create")) {
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;
 	}
 	
 	
